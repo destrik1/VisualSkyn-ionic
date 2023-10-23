bf2c2ebe00e41fb17ad8e9e9b631425175db230d
destrik1 <127449611+destrik1@users.noreply.github.com>
2023-10-23 14:56:06 -0300

    login

--- visual/skin/src/app/app-routing.module.ts
+++ visual/skin/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'para-ti',
+    redirectTo: 'splash',
     pathMatch: 'full'
   },
   {
--- visual/skin/src/app/login/login.page.ts
+++ visual/skin/src/app/login/login.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+formularioLogin:FormGroup;
+constructor(public fb: FormBuilder, public alertContrller: AlertController, public router: Router){
+
+  this.formularioLogin=this.fb.group({
+    'nombre': new FormControl("", Validators.required),
+    'password': new FormControl("", Validators.required)
+  })
+}
+
+
+
 
   ngOnInit() {
   }
+
+async ingresar(){
+  var f=this.formularioLogin.value;
+  var usuariRegisttro=JSON.parse(localStorage.getItem('usuario')!);
+  if(f.usuario==usuariRegisttro.nombre && f.password==usuariRegisttro.password){
+    localStorage.setItem("ingresado", "true");
+    this.router.navigate(["/home"]);
+
+  }else{
+    const alert=await this.alertContrller.create({
+      header: "usuario",
+      message:"Usuario no registrado",
+      buttons: ["ok"]
+    })
+    await alert.present();
+  }
+
+}
+
 
 }
